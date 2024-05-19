95a2062cfa4e92bb603ecba834dbbc1cf34e5bef
HemanthAchu <hemanthms186@gmail.com>
2024-05-20 00:14:03 +0530

    final

--- Controller.js/control.js
+++ Controller.js/control.js
@@ -1,63 +1,65 @@
-const projects =require('../model/model')
-exports.addUsers =async(req,res)=>{
-   
+const projects = require('../model/model')
+exports.addUsers = async (req, res) => {
+
    console.log(req.body);
-   
-try{
-console.log('inside');
-const { name, caloriesPerServing, cookTimeMinutes, cuisine, prepTimeMinutes } = req.body;
 
-   const newUser =new projects({
-     name,caloriesPerServing,caloriesPerServing,cookTimeMinutes,cuisine,prepTimeMinutes
-  })
-  //to store data to mongodb from mongoose model
-  
-  await newUser.save()
-  res.status(200).json(newUser)
+   try {
+      console.log('inside');
+      const { name, caloriesPerServing, cookTimeMinutes, cuisine, prepTimeMinutes } = req.body;
 
-  
-}catch(err){
-console.log(err);
+      const newUser = new projects({
+         name, caloriesPerServing, caloriesPerServing, cookTimeMinutes, cuisine, prepTimeMinutes
+      })
+      //to store data to mongodb from mongoose model
+
+      await newUser.save()
+      res.status(200).json(newUser)
+
+
+   } catch (err) {
+      console.log(err);
+   }
 }
-}
 
-exports.getData =async(req,res)=>{
-   try{
-       const AllData =await projects.find()
-       res.status(200).json(AllData)
+exports.getData = async (req, res) => {
+   try {
+      const AllData = await projects.find()
+      res.status(200).json(AllData)
 
-   }catch(err){
-  res.status(401).json(err)
+   } catch (err) {
+      res.status(401).json(err)
    }
 
-}  
+}
 
 
-exports.deleteData=async(req,res)=>{
-   const{id}=req.params
+exports.deleteData = async (req, res) => {
+   const { id } = req.params
    console.log(id);
-  try{ 
-   const deletedata = await projects.findByIdAndDelete(id)
-console.log("hem");
-   res.status(200).json(deletedata)
-  }catch(err){
-res.status(401).json(err)
-  }
+   try {
+      const deletedata = await projects.findByIdAndDelete(id)
+      console.log("hem");
+      res.status(200).json(deletedata)
+   } catch (err) {
+      res.status(401).json(err)
+   }
 
-   
+
 }
 
 
-exports.editUser =async(req,res)=>{
-   const userId =req.payload
+exports.editUser = async (req, res) => {
+   const {id}= req.params
+   console.log(id);
    const { name, caloriesPerServing, cookTimeMinutes, cuisine, prepTimeMinutes } = req.body;
-try{
- const updateUser = await projects.findByIdAndUpdate({_id:userId},{
-   name,caloriesPerServing,caloriesPerServing,cookTimeMinutes,cuisine,prepTimeMinutes
- })
- await updateUser.save()
- res.status(200).json(updateUser)
-}catch(err){
-res.status(401).json(err)
-}
+   try {
+      const updateUser = await projects.findByIdAndUpdate({ _id:id},
+          {
+         name, caloriesPerServing, caloriesPerServing, cookTimeMinutes, cuisine, prepTimeMinutes},
+          { new: true })
+      await updateUser.save()
+      res.status(200).json(updateUser)
+   } catch (err) {
+      res.status(401).json(err)
+   }
 }
--- routes/router.js
+++ routes/router.js
@@ -4,7 +4,7 @@ const projectController =require('../Controller.js/control')
 router.post('/add',projectController.addUsers)
 router.get('/get',projectController.getData)
 router.delete('/delete/:id',projectController.deleteData)
-router.put('/edit-user',projectController.editUser)
+router.put('/edit-project/:id',projectController.editUser)
 // // In your Express router
 // router.put('/edit/:id', projectController.editRecipe);
 
